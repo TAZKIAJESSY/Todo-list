c4af85ca6bf7317b4ec6c51ada6edf942c26fd72
tazkiajessy <jessy566eee@gmail.com>
2021-07-09 12:29:36 +0200

    main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,10 @@
 import React from "react";
+import TodoForm from "../component/TodoForm";
 
 export default function MainPage() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  );
 }
