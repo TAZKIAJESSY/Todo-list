852d4271467cc8f73e14a706a1b196d102e14359
tazkiajessy <jessy566eee@gmail.com>
2021-08-05 23:07:40 +0200

    edited

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
